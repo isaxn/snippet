4c74a010cf63bfa95955f01a8fe2140cb42cf563
isaxn <ihsanftdyroth@gmail.com>
2026-08-03 11:33:42 +0700

    Delete app directory

--- app/api/auth/logout/route.ts
+++
@@ -1,6 +0,0 @@
-import { destroySession } from "../../../auth"
-
-export async function POST() {
-  await destroySession()
-  return Response.json({ ok: true })
-}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShareCodeApp from "./sharecode-app";
-import { getSessionUser } from "./auth";
-
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const user = await getSessionUser();
-  return <ShareCodeApp user={user ? { name: user.name, email: user.email } : null} />;
-}
