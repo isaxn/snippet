d100d22ba05787ea1e2ccc3f03ea6769708bddb7
isaxn <ihsanftdyroth@gmail.com>
2026-08-03 10:03:27 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShareCodeApp from "./sharecode-app";
-import { getChatGPTUser } from "./chatgpt-auth";
-
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const user = await getChatGPTUser();
-  return <ShareCodeApp user={user ? { name: user.displayName, email: user.email } : null} />;
-}
